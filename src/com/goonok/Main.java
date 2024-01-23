d2b984a333dddc1658b26c6dd94c1914d2272578
alamin5g <21303134@iubat.edu>
2024-01-23 20:30:27 +0600

    Update the Pizza Delivery Bill

--- src/com/goonok/Main.java
+++ src/com/goonok/Main.java
@@ -1,7 +1,15 @@
-package com.goonok.pizza;
+package com.goonok;
+
+import com.goonok.pizza.Pizza;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Pizza Lover!");
+        Pizza pizza = new Pizza(false);
+        pizza.addExtraToppings();
+        pizza.addExtraCheese();
+        pizza.takeAway();
+        pizza.getBill();
+
     }
 }
--- src/com/goonok/pizza/Pizza.java
+++ src/com/goonok/pizza/Pizza.java
@@ -1,4 +1,58 @@
-package com.goonok;
+package com.goonok.pizza;
 
 public class Pizza {
+    private int price;
+    private boolean veg;
+    private int extraCheesePrice = 100;
+    private int extraToppingPrice = 150;
+    private int backPack = 20;
+    private boolean hasToppings = false;
+    private boolean hasChese = false;
+    private boolean hasBackPack = false;
+
+
+    public Pizza(boolean veg) {
+        this.veg = veg;
+        if (veg){
+            this.price  = 300;
+        }else {
+            this.price = 400;
+        }
+    }
+
+    public void addExtraCheese(){
+        System.out.println("Extra Cheese Added");
+        hasChese = true;
+    }
+
+    public void addExtraToppings(){
+        System.out.println("Extra topping added");
+        hasToppings = true;
+    }
+
+    public void takeAway(){
+        System.out.println("Take Away opted");
+        hasBackPack = true;
+    }
+
+    public void getBill(){
+        System.out.println("Pizza Bill = " + this.price);
+        int totalBill = this.price;
+        if (hasToppings){
+            System.out.println("Topping Bill = " + this.extraToppingPrice);
+            totalBill += extraToppingPrice;
+        }
+        if (hasChese){
+            System.out.println("Cheese Bill = " + this.extraCheesePrice);
+            totalBill += extraCheesePrice;
+        }
+        if (hasBackPack){
+            System.out.println("Package Bill = " + this.backPack);
+            totalBill += backPack;
+        }
+
+        System.out.println("--------------------------------");
+        System.out.println("Total Bill = " + totalBill);
+
+    }
 }
